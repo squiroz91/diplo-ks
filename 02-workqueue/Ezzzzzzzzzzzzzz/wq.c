5fbb35f98a46e35dbaf40e22919bb87820f78653
Ezzzzzzzzzzzzz <e_rojas@live.com>
2016-11-25 12:30:12 -0600

    Agrego tarea 02-Wq 0.02

--- 02-workqueue/Ezzzzzzzzzzzzzz/wq.c
+++ 02-workqueue/Ezzzzzzzzzzzzzz/wq.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
+#include <linux/slab.h>
 
 
 MODULE_LICENSE("GPL");
@@ -12,22 +13,23 @@ static struct workqueue_struct *my_wq; //Workqueue (workqueue_struct) creado de 
 typedef struct {
 
 	struct work_struct my_work;
-	int x;
+	int	x;
 
 } my_work_t;
 
 my_work_t *work, *work2;
 
 /*Handler*/
-static void my_wq_function(struct work_struct *work){ // Función que va a ser llamada
+static void my_wq_function( struct work_struct *work){ // Función que va a ser llamada
 
 	my_work_t *my_work = (my_work_t *)work;
-	printk("my_work.x %d\n", my_work->x);
-	kfree((void *)work); 
-	return;	
+	printk( "my_work.x %d\n", my_work->x );
+	kfree( (void *)work ); 
+	//return;	
 }
 
-int init_module(void){
+int i_module( void )
+{
 
 	int ret;
 	my_wq = create_workqueue("my_queue"); //Crea workqueue, que retorna a la referencia (workqueue_struct) 
@@ -38,9 +40,9 @@ int init_module(void){
 
 		if(work){
 
-			INIT_WORK((struct work_struct *)work, my_wq_function);	//Un camino para declarar de forma dinámica el trabajo (work)
+			INIT_WORK( (struct work_struct *)work, my_wq_function);	//Un camino para declarar de forma dinámica el trabajo (work)
 			work->x = 1;
-			ret = queue_work(my_wq, (struct work_struct *)work);	//Encolar trabajos a dicha cola
+			ret = queue_work( my_wq, (struct work_struct *)work );	//Encolar trabajos a dicha cola
 		}
 
 		printk("Work 1 created\n");
@@ -61,13 +63,14 @@ int init_module(void){
 return 0;
 }
 
-void cleanup_module(void){		//Finalizar trabajos 
+void clean_module( void )
+{		//Finalizar trabajos 
 			
-	flush_workqueue(my_wq); 	// Para sacar todas las tareas pendientes, y salir limpiamente de alguna rutina del kernel
-	destroy_workqueue(my_wq);
-	printk(KERNEL_ALERT "Finished Workqueue\n");
-	return;
+	flush_workqueue( my_wq ); 	// Para sacar todas las tareas pendientes, y salir limpiamente de alguna rutina del kernel
+	destroy_workqueue( my_wq );
+	printk(KERN_ALERT "Finished Workqueue\n");
+	//return;
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(i_module);
+module_exit(clean_module);
